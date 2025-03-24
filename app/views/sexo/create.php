5da93c451220f8aef83a4eab96b5123d57176e69
STALIN FRANCIS <educaysoft@educaysoft.org>
2025-03-24 17:11:44 -0500

    Update create.php

--- app/views/sexo/create.php
+++ app/views/sexo/create.php
@@ -1,5 +1,5 @@
 <form action="../../controllers/SexoController.php?action=create" method="POST">
-    <label for="nombre">Nombre:</label>
+    <label for="nombre">Nombre del Sexo:</label>
     <input type="text" name="nombre" id="nombre" required>
     <input type="submit" value="Crear">
 </form>
