4895d744973cb4152f84ce028aab7f5a1e93b241
orejuelawilton <orejuelawilton@gmail.com>
2025-05-12 21:35:53 -0500

    mejoras

--- app/views/persona/index.php
+++ app/views/persona/index.php
@@ -32,7 +32,7 @@
                         <td><?php echo htmlspecialchars($persona['nombres']); ?></td>
                         <td><?php echo htmlspecialchars($persona['apellidos']); ?></td>
                         <td><?php echo htmlspecialchars($persona['fechanacimiento']); ?></td>
-                        <td><?php echo htmlspecialchars($persona['sexo']); ?></td> <td><?php echo htmlspecialchars($persona['estadocivil']); ?></td> <td>
+                        <td><?php echo htmlspecialchars($persona['elsexo']); ?></td> <td><?php echo htmlspecialchars($persona['elestadocivil']); ?></td> <td>
                             <a href="/whatsapp6a/public/persona/editForm?id=<?php echo htmlspecialchars($persona['idpersona']); ?>">
                                 <button>Editar</button>
                             </a>
