f5687b5d220e506fb744402ba6454f046cfbc167
Heidy Jordan <112960628+Nallely20030627@users.noreply.github.com>
2025-05-22 16:58:31 -0500

    Update delete.php

--- app/views/direccion/delete.php
+++ app/views/direccion/delete.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Elininar Direccion</title>
+    <title>Elininar Sexo</title>
 </head>
 <body>
 
-<h1>Elininar Direccion</h1>
+<h1>Editar el Sexo</h1>
 <form action="/whatsapp6a/public/sexo/delete" method="POST">
-    <input type="hidden" name="iddireccion" value="<?php echo htmlspecialchars($direccion['iddireccion']); ?>">
+    <input type="hidden" name="idsexo" value="<?php echo htmlspecialchars($sexo['idsexo']); ?>">
     
     <label for="nombre">Nombre:</label>
-    <input type="text" name="nombre" id="nombre" value="<?php echo htmlspecialchars($direccion['nombre']); ?>" required>
+    <input type="text" name="nombre" id="nombre" value="<?php echo htmlspecialchars($sexo['nombre']); ?>" required>
     
     <input type="submit" value="Eliminar">
 </form>
